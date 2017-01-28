8391ac19d39440df38dcadf6c669993c3e5e5413
kalin-marinov <kalimar94@gmail.com>
2017-01-28 17:52:34 +0200

    Adding mongodb data layer

---
+++ WordChainGame/src/WordChainGame.Data/Class1.cs
@@ -0,0 +1,55 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WordChainGame.Data
+{
+    public class Class1
+    {
+        public Class1()
+        {
+        }
+
+        public static void Main()
+        {
+            var client = new MongoClient("mongodb://localhost:27017");
+
+            var db = client.GetDatabase("Test");
+
+            BsonClassMap.RegisterClassMap<Topic>(cm =>
+            {
+                cm.AutoMap();
+            });
+
+            var col = db.GetCollection<Topic>("Topics").AsQueryable().ToList();
+
+            var test = db.GetCollection<Topic>("TestCollection");
+
+            Console.WriteLine("there");
+        }
+    }
+
+    public class Topic
+    {
+        [BsonId]
+        public ObjectId Id { get; set; }
+
+        public string Name { get; set; }
+
+        public ICollection<Word> Words { get; set;}
+    }
+
+    public class Word
+    {
+        public string Value { get; set; }
+
+        public string Author { get; set; }
+
+        
+    }
+}
---
+++ WordChainGame/src/WordChainGame.Data/TopicsManager.cs
@@ -0,0 +1,48 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WordChainGame.Data
+{
+    public class TopicsManager
+    {
+        private IMongoDatabase database;
+
+        public TopicsManager(IMongoDatabase database)
+        {
+            this.database = database;
+        }
+
+        public void AddTopic(string name)
+        {
+            var topic = new Topic { Name = name };
+            var topics = database.GetCollection<Topic>("Topics");
+            topics.InsertOne(topic);
+        }
+
+        public IEnumerable<Word> GetWords(string topic)
+        {
+            var topics = database.GetCollection<Topic>("Topics");
+            var topicData = topics.Find(t => t.Name == topic).Single();
+
+            return topicData.Words;
+        }
+
+        public void AddWord(string topic, string word, string author)
+        {
+            var topics = database.GetCollection<Topic>("Topics");
+            var topicData = topics.Find(t => t.Name == topic).Single();
+            var lastWord = topicData.Words.Last().Value;
+
+            if (word.Last() != lastWord.First())
+            {
+                throw new ArgumentException("the new word must start with the last letter of the first word");
+            }
+
+            topicData.Words.Add(new Word { Value = word, Author = author });
+            topics.UpdateOne(t => t.Name == topic, topic);
+        }
+    }
+}
